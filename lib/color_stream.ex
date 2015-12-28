bcb21429931d4db41b0e815253aadc5679ffc3f1
Nick Veys <nick@codelever.com>
2015-12-28 14:19:22 -0600

    Adding rgb_to_hex

--- lib/color_stream.ex
+++ lib/color_stream.ex
@@ -1,2 +1,11 @@
 defmodule ColorStream do
+
+  def rgb_to_hex(r, g, b) when r in 0..255 when g in 0..255 when b in 0..255 do
+    "~2.16.0B"
+    |> List.duplicate(3)
+    |> Enum.join
+    |> :io_lib.format([r, g, b])
+    |> to_string
+  end
+  def rgb_to_hex({r, g, b}), do: rgb_to_hex(r, g, b)
 end
--- test/color_stream_test.exs
+++ test/color_stream_test.exs
@@ -2,7 +2,19 @@ defmodule ColorStreamTest do
   use ExUnit.Case
   doctest ColorStream
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
+  [
+    [{0, 0, 0}, "000000"],
+    [{1, 1, 1}, "010101"],
+    [{100, 100, 100}, "646464"],
+    [{1, 128, 250}, "0180FA"],
+    [{255, 0, 255}, "FF00FF"],
+    [{0, 128, 0}, "008000"],
+  ]
+  |> Enum.each(fn ([triplet, expected]) ->
+    @triplet triplet
+    @expected expected
+    test "rgb_to_hex(#{inspect(@triplet)}) converts to #{@expected}" do
+      assert ColorStream.rgb_to_hex(@triplet) == @expected
+    end
+  end)
 end
